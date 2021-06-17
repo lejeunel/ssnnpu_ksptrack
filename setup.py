c5eca2b7f0f0865e1800bb49c3f308f250666e37
olol85 <olol85@gmail.com>
2021-06-17 17:55:46 +0200

    readme and requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(
-    name='ksptrack',
-    version='1.0',
-    description='KSPTrack Labeling',
-    packages=find_packages()
-)
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
+
+setup(name='ksptrack',
+      version='1.0',
+      description='KSPTrack Labeling',
+      install_requires=install_requires,
+      packages=find_packages())
